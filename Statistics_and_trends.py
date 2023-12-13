b3c7994fb3572758ac21feaea9cdab1f4ee827d6
ASHIKAMOHAN <ashikaadhi99@gmail.com>
2023-12-13 19:22:49 +0000

    Docstring updated

--- Statistics_and_trends.py
+++ Statistics_and_trends.py
@@ -8,7 +8,7 @@ from sklearn.utils import resample
 
 # paths to datasets
 # Link for original world bank dataset https://datacatalog.worldbank.org/search/dataset/0037712
-Original_dataUri = "C:\\Users\\ashik\\Desktop\\WDICSV.csv"
+Original_dataUri = 'WDICSV.csv'
 pivoted_data_uri = 'PivotedDataset.csv'
 cleaned_data_uri = 'CleanedDataset1.csv'
 
@@ -64,6 +64,12 @@ countryMap = {
 # Reading data set using pandas.
 # use of melt and pivot methods in dataframe.
 def getDataset(dataUrl):
+        """ Function to  read a dataset and Transpose using pandas
+    Args:
+        dataUrl (uri): This argument takes a path to the dataset 
+     Returns:
+        Returns a transposed dataframe after melt and pivot operations on the dataUrl that was passed.
+    """
     dataset = pd.read_csv(dataUrl)
     melted_df = dataset.melt(id_vars=[
         'Country Name', 'Country Code', 'Indicator Name', 'Indicator Code'], var_name='Year', value_name='Value')
@@ -72,6 +78,14 @@ def getDataset(dataUrl):
     Transposed_df.to_csv('PivotedDataset.csv')
     return Transposed_df
 def cleanDataSet(dataset):
+        """ Function to clean a dataset
+
+    Args:
+        dataset: Dataset which needs to be cleaned is passed as an argument.
+
+    Returns:
+        _Returns a cleaned dataset after removing null values and indicators which are not useful.
+    """
     filtered_columns = [col for col in dataset.columns if col in INDICATOR_CODES]
     df_filtered = getDataset(Original_dataUri)[filtered_columns]
     df_cleaned = df_filtered.fillna(df_filtered.mean())
@@ -94,12 +108,32 @@ df_selected = df_filtered[df_filtered['Country Name'].isin(selected_countries)]
 
 # Function to calculate skewness and kurtosis for a given indicator
 def calculate_skewness_kurtosis(data):
+    """
+    Calculate skewness and kurtosis for a given dataset.
+
+    Parameters:
+    - data (array-like): The input data for which skewness and kurtosis will be calculated.
+
+    Returns:
+    - tuple: A tuple containing two values - skewness and kurtosis.
+    """
     skewness = skew(data)
     kurt = kurtosis(data)
     return skewness, kurt
 
 # Function to perform bootstrapping and confidence intervals
 def bootstrap(data, num_iterations=1000, confidence_level=0.95):
+    """
+    Perform bootstrapping on a given dataset to estimate confidence intervals for skewness and kurtosis.
+
+    Parameters:
+    - data (array-like): The input data for bootstrapping.
+    - num_iterations (int, optional): The number of bootstrap iterations. Default is 1000.
+    - confidence_level (float, optional): The confidence level for calculating intervals. Default is 0.95.
+
+    Returns:
+    - numpy.ndarray: A 2D array containing the confidence intervals for skewness and kurtosis.
+    """
     results = []
     for _ in range(num_iterations):
         # Generate a bootstrap sample
@@ -199,6 +233,23 @@ plot_correlation_heatmap(df_filtered, "Singapore", selected_indices, feature_map
 # Plot correlation matrix and heatmap for India
 plot_correlation_heatmap(df_filtered, "India", selected_indices, feature_map, country_map, 3)
 def plot_grouped_bar_chart(df, countries, years, indicator_column, title, xlabel, ylabel, palette, figure_number):
+        """
+    Plot a grouped bar chart to visualize the specified indicator for selected countries over different years.
+
+    Parameters:
+    - df (pandas.DataFrame): The DataFrame containing the data.
+    - countries (list): A list of country names to be included in the chart.
+    - years (list): A list of years to be included in the chart.
+    - indicator_column (str): The column name representing the indicator to be plotted.
+    - title (str): The title of the chart.
+    - xlabel (str): The label for the x-axis.
+    - ylabel (str): The label for the y-axis.
+    - palette (str or seaborn color palette): The color palette to be used in the chart.
+    - figure_number (int): The figure number for saving the chart.
+
+    Returns:
+    - None: Displays the grouped bar chart."""
+    
     filtered_data = df[
         (df['Country Name'].isin(countries)) &
         (df['Year'].isin(years))
@@ -251,6 +302,17 @@ plot_grouped_bar_chart(
     2
 )
 def plot_gdp_scatter(df, country_names, indicator_column):
+        """
+    Generate a scatter plot to compare GDP per capita growth between specified countries.
+
+    Parameters:
+    - df (pandas.DataFrame): The DataFrame containing the data.
+    - country_names (list): A list of country names to be compared in the scatter plot.
+    - indicator_column (str): The column name representing the GDP per capita growth indicator.
+
+    Returns:
+    - None: Displays the scatter plot.
+        """
     plt.figure(figsize=(10, 6))
 
     for country_name in country_names:
@@ -284,6 +346,19 @@ country_names = ['United Kingdom', 'United States']
 # Call the function
 plot_gdp_scatter(df_filtered, country_names, indicator_column)
 def plot_indicators_for_country(ax, df, country_name, selected_indicators, indicator_labels):
+    """
+    Plot line graphs to compare specific economic indicators for a given country over time.
+
+    Parameters:
+    - ax (matplotlib.axes.Axes): The Axes object on which to plot the data.
+    - df (pandas.DataFrame): The DataFrame containing the data.
+    - country_name (str): The name of the country for which the indicators will be plotted.
+    - selected_indicators (list): A list of column names representing the indicators to be plotted.
+    - indicator_labels (list): A list of labels for the corresponding indicators.
+
+    Returns:
+    - None: Displays the line graphs on the specified Axes object.
+    """
     # Selecting the data for a specific country
     country_data = df[df['Country Name'] == country_name]
 
